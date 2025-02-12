48457c35ec94663156f61e676b1ef5868e7f478d
Bhargavi <Bhargavi@DESKTOP-EFAPS9H>
2025-02-12 12:17:09 +0530

    written test scripts and modified

--- Selenium_newProject/src/test/java/contactsPage/ContactsTest.java
+++ Selenium_newProject/src/test/java/contactsPage/ContactsTest.java
@@ -4,6 +4,7 @@ public class ContactsTest {
 
 	public static void main(String[] args) {
 		System.out.println("This is my contacts page");
+		System.out.println("----------");
 	}
 
 }
--- Selenium_newProject/src/test/java/homePage/HomeTest.java
+++ Selenium_newProject/src/test/java/homePage/HomeTest.java
@@ -4,6 +4,7 @@ public class HomeTest {
 
 	public static void main(String[] args) {
 		System.out.println("This is my homepage");
+		System.out.println("-----------");
 	}
 
 }
