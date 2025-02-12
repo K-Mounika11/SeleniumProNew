ab6a883d561b9a8f0a275489c4c590485c766653
Kamini Pise <kaminipise0035@gmail.com>
2025-02-12 11:42:11 +0530

    Sharing project

---
+++ Selenium_newProject/src/test/java/contactsPage/ContactsTest.java
@@ -0,0 +1,9 @@
+package contactsPage;
+
+public class ContactsTest {
+
+	public static void main(String[] args) {
+		System.out.println("This is my contacts page");
+	}
+
+}
---
+++ Selenium_newProject/src/test/java/homePage/HomeTest.java
@@ -0,0 +1,9 @@
+package homePage;
+
+public class HomeTest {
+
+	public static void main(String[] args) {
+		System.out.println("This is my homepage");
+	}
+
+}
